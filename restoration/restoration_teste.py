513ea6bce695c835a24ffd593631e1456ddce96f
Izabel Ferrari <ferrari.izabel@gmail.com>
2018-11-21 20:52:21 -0200

    Restoration test

--- restoration/restoration_teste.py
+++ restoration/restoration_teste.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class Restoration:
 
-    def run_restoration(img):
+    def run_restoration(self, img):
         img_temp = np.copy(img)
         img_temp[0:100] = 0
         return img_temp
