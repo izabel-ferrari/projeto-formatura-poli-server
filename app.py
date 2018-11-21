b45ba8ffc208be34d1394716c7c0d1caad0b64ef
Izabel Ferrari <ferrari.izabel@gmail.com>
2018-11-21 18:33:06 -0200

    Test

--- app.py
+++ app.py
@@ -1,32 +1,62 @@
-from flask import Flask, request, Response, render_template
-import cv2
 import os
-import json
+
+from flask import Flask, request, render_template, send_from_directory
+
+__author__ = 'ibininja'
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = './upload'
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 
 @app.route("/")
-def hello_world():
-    return render_template('account.html')
+def index():
+    return render_template("upload.html")
 
-@app.route("/upload", methods=['POST'])
-def upload_file():
-	file = request.files['image']
-	filename = file.filename
-	filepath = app.config['UPLOAD_FOLDER']
-	if not os.path.exists(filepath):
-		os.makedirs(filepath)
-	file.save(os.path.join(filepath, file.filename))
-	img = cv2.imread(os.path.join(filepath, file.filename))
-	canny = findLines.processImage(img)
-	newLines, lines = findLines.getLines(canny)
-	data = {}
-	data['lines_coord'] = newLines
-	json_data = json.dumps(data)
 
-	return Response(json_data)
+@app.route("/upload", methods=["POST"])
+def upload():
+    folder_name = request.form['superhero']
+    '''
+    # this is to verify that folder to upload to exists.
+    if os.path.isdir(os.path.join(APP_ROOT, 'files/{}'.format(folder_name))):
+        print("folder exist")
+    '''
+    target = os.path.join(APP_ROOT, 'files/{}'.format(folder_name))
+    print(target)
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    print(request.files.getlist("file"))
+    for upload in request.files.getlist("file"):
+        print(upload)
+        print("{} is the file name".format(upload.filename))
+        filename = upload.filename
+        # This is to verify files are supported
+        ext = os.path.splitext(filename)[1]
+        if (ext == ".jpg") or (ext == ".png"):
+            print("File supported moving on...")
+        else:
+            render_template("Error.html", message="Files uploaded are not supported...")
+        destination = "/".join([target, filename])
+        print("Accept incoming file:", filename)
+        print("Save it to:", destination)
+        upload.save(destination)
+
+    # return send_from_directory("images", filename, as_attachment=True)
+    return render_template("complete.html", image_name=filename)
+
+
+@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory("images", filename)
+
+
+@app.route('/gallery')
+def get_gallery():
+    image_names = os.listdir('./images')
+    print(image_names)
+    return render_template("gallery.html", image_names=image_names)
+
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=4555, debug=True)
---
+++ app2.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, Response, render_template
+import cv2
+import os
+import json
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './upload'
+
+@app.route("/")
+def hello_world():
+    return render_template('account.html')
+
+@app.route("/upload", methods=['POST'])
+def upload_file():
+	file = request.files['image']
+	filename = file.filename
+	filepath = app.config['UPLOAD_FOLDER']
+	if not os.path.exists(filepath):
+		os.makedirs(filepath)
+	file.save(os.path.join(filepath, file.filename))
+	img = cv2.imread(os.path.join(filepath, file.filename))
+	canny = findLines.processImage(img)
+	newLines, lines = findLines.getLines(canny)
+	data = {}
+	data['lines_coord'] = newLines
+	json_data = json.dumps(data)
+
+	return Response(json_data)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
